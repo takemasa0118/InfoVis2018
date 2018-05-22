7cd04a7d46876165b6f0475cc17d225a63a01783
山本　孟正 <1575102t@istc-a-057.epc.center.kobe-u.ac.jp>
2018-05-22 12:50:03 +0900

    w10

---
+++ w10/task4/w10_main_ex02.js
@@ -0,0 +1,43 @@
+function main()
+{
+    var volume = new KVS.LobsterData();
+    var screen = new KVS.THREEScreen();
+    var scene = new THREE.Scene();
+
+    screen.init( volume, {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        enableAutoResize: false
+    });
+
+    var bounds = Bounds( volume );
+    screen.scene.add( bounds );
+
+    var point = new THREE.Vector3(60,60,17);
+    var normal = new THREE.Vector3(1,0,4);
+    var surfaces = Isosurfaces( volume, point , normal );
+    screen.scene.add( surfaces );
+
+    document.addEventListener( 'mousemove', function() {
+        screen.light.position.copy( screen.camera.position );
+    });
+
+    window.addEventListener( 'resize', function() {
+        screen.resize( [ window.innerWidth, window.innerHeight ] );
+    });
+
+   
+    var light = new THREE.PointLight();
+    light.position.set( 5, 5, 5 );
+    screen.scene.add( light );
+    
+  
+    var material = new THREE.ShaderMaterial({
+        vertexColors: THREE.VertexColors,
+        vertexShader: document.getElementById('gouraud.vert').text,
+        fragmentShader: document.getElementById('gouraud.frag').text,
+    });
+
+    
+    screen.loop();
+}
